d340fe596a7efc81006451e4140688392abdba46
Kouya Arashiro <a.kouya2001@gmail.com>
2024-07-18 20:18:48 +0900

    Fix start window

--- main.sh
+++ main.sh
@@ -1,29 +1,28 @@
 #! /bin/bash
 
-echo "Hello, CalcQuest!"
-echo "Please select a level"
+echo " Hello, CalcQuest!"
+echo " Please select a level"
 
-echo "1: Single-Single Digits"
-echo "2: Single-Double Digits"
-echo "3: Double-Double Digits"
-echo "4: Double-Triple Digits"
-echo "5: Triple-Triple Digits"
+echo "   1: Single-Single Digits"
+echo "   2: Single-Double Digits"
+echo "   3: Double-Double Digits"
+echo "   4: Double-Triple Digits"
+echo "   5: Triple-Triple Digits"
 
 while :; do
-   read -p "Level: " level
+   read -p " Level: " level
    if (( $level < 1 || $level > 5 )); then
-      echo "Please select a level (1/2/3/4/5) "  
+      echo " Please select a level (1/2/3/4/5) "  
    else
       break
    fi
 done
 
-echo "Level: "$level
-echo "Are you ready? (Enter) "
+echo " Are you ready? (Enter) "
 read REPLY
 seconds=3
 while [ $seconds -gt 0 ]; do
-   echo -ne $seconds
+   echo -ne "          $seconds\r"
    sleep 1
    ((seconds--))
 done
