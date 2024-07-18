88cd6438ac19432d6eab63d09bc30c0b0e6f5e09
Kouya Arashiro <a.kouya2001@gmail.com>
2024-07-18 09:09:46 +0900

    Add start window

---
+++ main.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Hello, CalcQuest!"
+echo "Please select a level"
+
+echo "1: Single-Single Digits"
+echo "2: Single-Double Digits"
+echo "3: Double-Double Digits"
+echo "4: Double-Triple Digits"
+echo "5: Triple-Triple Digits"
+
+read -p "Level: " level
+echo $level
