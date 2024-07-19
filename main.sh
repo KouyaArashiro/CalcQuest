d951b36d33608240d2f350c882e531719d163d90
Kouya Arashiro <a.kouya2001@gmail.com>
2024-07-19 16:55:58 +0900

    Add generate_question func

--- main.sh
+++ main.sh
@@ -1,5 +1,27 @@
 #! /bin/bash
 
+generate_problem() {
+    local level=$1
+    local number_of_questions=10
+    local num1 num2 operator_num
+
+    operators=(+ - x /)
+
+
+    for i in `seq 1 $number_of_questions` ; do
+        case $level in
+           1) num1=$((RANDOM % 10)); num2=$((RANDOM % 10)); operator_num=$((RANDOM % 4)) ;;
+           2) num1=$((RANDOM % 10)); num2=$((RANDOM % 90 + 10)); operator_num=$((RANDOM % 4)) ;;
+           3) num1=$((RANDOM % 90 + 10)); num2=$((RANDOM % 90 + 10)); operator_num=$((RANDOM % 4)) ;;
+           4) num1=$((RANDOM % 90 + 10)); num2=$((RANDOM % 900 + 100)); operator_num=$((RANDOM % 4)) ;;
+           5) num1=$((RANDOM % 900 + 100)); num2=$((RANDOM % 900 + 100)); operator_num=$((RANDOM % 4)) ;;
+
+        esac
+        echo $num1 ${operators[$operator_num]} $num2 >> questions.dat
+    done
+
+}
+
 echo " Hello, CalcQuest!"
 echo " Please select a level"
 
@@ -18,6 +40,8 @@ while :; do
    fi
 done
 
+generate_problem $level
+
 echo  -n " Are you ready? (Enter) "
 read -r REPLY
 seconds=3
@@ -26,3 +50,10 @@ while [ $seconds -gt 0 ]; do
    sleep 1
    ((seconds--))
 done
+
+start_time=$(date +%s)
+#user_answer=get_answer problem
+end_time=$(date +%s)
+
+#scoreing user_answer
+#show_history
